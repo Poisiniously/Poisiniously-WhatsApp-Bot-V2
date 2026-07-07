2fd9aa9c2ac1ae29761fb53fa521a64ee7ab726f
Poisiniously <vbozjak@Icloud.com>
2026-07-07 10:24:40 +0200

    Delete tools directory

--- tools/hello/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'hello',
-    description: 'Greets the user',
-    async execute(sock, msg, args) {
-        const remoteJid = msg.key.remoteJid;
-        await sock.sendMessage(remoteJid, { text: '👋 Hello there! How can I help you today?' });
-    }
-};
--- tools/ping/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Responds with pong!',
-    async execute(sock, msg, args) {
-        const remoteJid = msg.key.remoteJid;
-        await sock.sendMessage(remoteJid, { text: '🏓 Pong!' });
-    }
-};
